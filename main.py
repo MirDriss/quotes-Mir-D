0e080a34976057c20d4306c3b023f6c10986f4ca
issonotakashi <104083364+issonotakashi@users.noreply.github.com>
2023-05-23 12:53:12 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,27 +1,28 @@
+from functions import *
+
 def menu():
     print("\n==== Programming Quotes ====")
-    print("1. Random quote")
-    print("2. Display a certain number of quotes")
-    print("3. Add a quote")
-    print("4. Exit")
+    print("random : Random quote")
+    print("display : Display quotes")
+    print("add : Add a new quote")
+    print("exit : Exit the program")
 
 def main():
     while True:
         quotes = load_quotes("quotes.txt")
         menu()
 
-        choice = input("Choose an action (1-4): ")
-
-        if choice == "1":
+        choice = input(">> ")
+        
+        if choice == "random":
             print_quote(random_quote(quotes))
-        elif choice == "2":
+        elif choice == "display":
             count = int(input("Enter the number of quotes to display: "))
             display_quotes(quotes, count)
-        elif choice == "3":
+        elif choice == "add":
             add_quote(quotes, "quotes.txt")
-            print("Quote added successfully!")
-        elif choice == "4":
-            print("Goodbye...")
+        elif choice == "exit":
+            print("Good bye...")
             break
         else:
             print("Invalid input")
